6844ec1bce06ecc45b3d0c18b90f6568f330d320
Noelia Root <noeliakroot@gmail.com>
2023-12-13 07:10:56 -0700

    fininshed form and UI things. Navbar background still really funky

---
+++ Recipes/MealInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  MealInfoView.swift
+//  Recipes
+//
+//  Created by Noelia Root on 12/12/23.
+//
+
+import SwiftUI
+
+struct MealInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealInfoView()
+}
---
+++ Recipes/View+Encapsulate.swift
@@ -0,0 +1,8 @@
+//
+//  View+Encapsulate.swift
+//  Recipes
+//
+//  Created by Noelia Root on 12/12/23.
+//
+
+import Foundation
